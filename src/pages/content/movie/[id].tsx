11ac9df11c3c513cc26d7a9879e9c206d8fb2547
github akito-10 <akt.fukuda@gmail.com>
2021-10-05 23:12:26 +0900

    より抽象的なロジックに書き換え

--- src/pages/content/movie/[id].tsx
+++ src/pages/content/movie/[id].tsx
@@ -2,33 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import Youtube from "react-youtube";
 import MainLayoutTemplate from "components/templates/MainLayoutTemplate";
-import ContentArea from "components/atoms/ContentArea";
-
-interface Movie {
-  id: number;
-  video: string;
-}
 
 interface Options {
   width: string;
   height: string;
 }
 
-const movies: Movie[] = [
-  {
-    id: 1,
-    video: "LZ-IuDos9Kk",
-  },
-  {
-    id: 2,
-    video: "-eMKayQqkK0",
-  },
-];
-
 const MovieContent = () => {
   const router = useRouter();
-  const [movie, setMovie] = useState<Movie>();
-  const id = Number(router.query.id);
+  const id = router.query.id;
   const movieContainerRef = useRef<HTMLDivElement>(null);
   const [opts, setOpts] = useState<Options>();
   const [paddings, setPaddings] = useState<number>(32);
@@ -46,15 +28,11 @@ const MovieContent = () => {
     });
   }, [opts]);
 
-  useEffect(() => {
-    setMovie(movies.find((movie) => movie.id === id));
-  }, [id]);
-
   return (
     <MainLayoutTemplate page="解説動画" description="解説動画ページです。">
       <div className="px-4 xl:px-12 w-full xl:w-2/3" ref={movieContainerRef}>
-        {movie ? (
-          <Youtube className="mt-6" videoId={movie.video} opts={opts} />
+        {typeof id === "string" && opts ? (
+          <Youtube className="mt-6" videoId={id} opts={opts} />
         ) : null}
       </div>
     </MainLayoutTemplate>
